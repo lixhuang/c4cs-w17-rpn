15da3a42881ccec8180400ebfa9e4642e7f2c4f0
ys <lixhuang@umich.edu>
2017-03-10 12:10:15 -0800

    init

---
+++ rpn.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import operator
+
+OPERATIONS = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+}
+
+def calculate(arg):
+    stack = []
+    for operand in arg.split():
+        try:
+            operand = float(operand)
+            stack.append(operand)
+        except:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            operator_fn = OPERATIONS[operand]
+            result = operator_fn(arg1, arg2)
+
+            stack.append(result)
+    return stack.pop()
+
+def main():
+    while True:
+        rst = calculate(input("rpn calc> "))
+        print (rst)
+
+if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
+    main()
+
