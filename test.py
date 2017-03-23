e2356729c11bfccf2c2de25fe3fab618236ccecb
ys <lixhuang@umich.edu>
2017-03-23 13:45:36 -0700

    add dependency

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ result = rpn.calculate("1 1 +")
 assert(2 == result)
 result = rpn.calculate("2 0 -")
 assert(2 == result)
-#    def test_exp(self):
-#        result = rpn.calculate("2 0 ^")
-#        self.assertEqual(1, result)
+result = rpn.calculate("2 0 ^")
+assert(1 == result)
 
