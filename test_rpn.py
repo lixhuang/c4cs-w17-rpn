cbd4237ae16c75ce4f8121d4769b4b0d2852bb9d
ys <lixhuang@umich.edu>
2017-03-22 19:35:42 -0700

    remove testcase

--- test_rpn.py
+++ test_rpn.py
@@ -7,7 +7,7 @@ class TestBasics(unittest.TestCase):
     def test_subtract(self):
         result = rpn.calculate("2 0 -")
         self.assertEqual(2, result)
-    def test_subtract(self):
-        result = rpn.calculate("2 0 ^")
-        self.assertEqual(1, result)
+#    def test_exp(self):
+#        result = rpn.calculate("2 0 ^")
+#        self.assertEqual(1, result)
 
